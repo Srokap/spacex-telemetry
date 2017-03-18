73a028420a11afdedc151832cfd8bc9b85999b3c
Paweł Sroka <srokap@gmail.com>
2017-03-18 02:30:00 +0100

    Redone crs10 capture. Added echostar23 captured data. Minor refactoring.

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const {
-    desktopCapturer
-} = require('electron');
+// "use strict"
 
+const {
+    desktopCapturer,
+    ipcRenderer
+} = require('electron')
+
+let params
+    , ws
+    , classifierContext
+    , characters = []
+    , glyphs = '0123456789.:'.split('')
+    , socket
+    , startCollection = false
+
+function log(data) {
+    document.querySelector('#debug').innerHTML = JSON.stringify(data, null, 2)
+}
+
+ipcRenderer
+    .on('params', (event, arg) => {
+        console.log('params', event, arg)
+        params = arg
+        initProcessing()
+    })
+
+ipcRenderer.send('started')
 
 let fpsCnt = 0
+    , procFpsCnt = 0 // processed screenshots per second
 function fpsTick() {
     fpsCnt++
     window.requestAnimationFrame(fpsTick)
@@ -11,56 +35,64 @@ function fpsTick() {
 fpsTick()
 var fpsInterval = setInterval(() => {
     // do the thing with fps
-    document.getElementById('fps').innerHTML = fpsCnt + ' FPS'
+    document.getElementById('fps').innerHTML = fpsCnt + ' FPS ' + procFpsCnt + ' SPS'
     fpsCnt = 0
+    procFpsCnt = 0
 }, 1000)
 
+function initProcessing() {
+    console.log('Waiting for 00:00:00...')
 
-var socket = initWebSocket();
-getOwnWindow().then(win => {
-    let video = document.querySelector('video');
-    return init(win, video);
-}).catch(e => {
-    console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'));
-}).then(video => {
-    //give a name and a bounding box
-    //giving x,y,width and height
-    loadChars();
+    if (params.useWebsocket) {
+        socket = initWebSocket()
+    }
+    getOwnWindow().then(win => {
+        let video = document.querySelector('video')
+        return init(win, video)
+    }).catch(e => {
+        console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'))
+    }).then(video => {
+        //give a name and a bounding box
+        //giving x,y,width and height
+        loadChars()
 
-    initCapture(video, {
-        'time': [100, 44, 200, 23],
-        'speed': [70, 224, 105, 23],
-        'altitude': [265, 222, 105, 23]
-    });
-});
+        initCapture(video, {
+            'time': [100, 44, 200, 23],
+            'speed': [70, 224, 105, 23],
+            'altitude': [265, 222, 105, 23]
+        })
+    })
+
+    classifierContext = document.querySelector('#classifier').getContext('2d')
+}
 
 /**
  * gets the application window handle
  */
 function getOwnWindow() {
-    console.log('getting capture stream');
+    console.log('getting capture stream')
     return new Promise((resolve, reject) => {
         desktopCapturer.getSources({
             types: ['window']
-        }, function(error, sources) {
+        }, function (error, sources) {
             if (error) {
-                console.log('error getting capture stream');
-                reject(error);
+                console.log('error getting capture stream')
+                reject(error)
             } else {
-                resolve(sources.filter(s => s.name === document.title)[0]);
+                resolve(sources.filter(s => s.name === document.title)[0])
             }
-        });
+        })
 
-    });
+    })
 }
 
 /**
  * inits the video capture and streams to video element
  */
 function init(win, video) {
-    console.log('initing media stream to video element');
+    console.log('initing media stream to video element')
     return new Promise((resolve, reject) => {
-        console.log("Desktop sharing started.. desktop_id:" + win.id);
+        console.log("Desktop sharing started.. desktop_id:" + win.id)
         navigator.webkitGetUserMedia({
             audio: false,
             video: {
@@ -73,11 +105,11 @@ function init(win, video) {
                     maxHeight: window.outerHeight,
                 }
             }
-        }, gotStream, reject);
+        }, gotStream, reject)
 
         function gotStream(stream) {
-            video.src = URL.createObjectURL(stream);
-            resolve(video);
+            video.src = URL.createObjectURL(stream)
+            resolve(video)
         }
     })
 }
@@ -96,31 +128,31 @@ function initCapture(video, slices) {
      *     name: the buffer name
      * }
      */
-    var buffers = document.querySelector('#buffers');
+    var buffers = document.querySelector('#buffers')
     var data = Object.keys(slices).map(name => {
-        var selector = '#'+name;
-        var el = document.createElement('canvas');
-        buffers.appendChild(el);
-        var boundingBox = slices[name];
-        el.width = boundingBox[2];
+        var selector = '#' + name
+        var el = document.createElement('canvas')
+        buffers.appendChild(el)
+        var boundingBox = slices[name]
+        el.width = boundingBox[2]
         //two times the height to be able to draw a backbuffer
-        el.height = boundingBox[3] * 2;
-        var ctx = el.getContext('2d');
+        el.height = boundingBox[3] * 2
+        var ctx = el.getContext('2d')
         return {
             el,
             ctx,
             boundingBox,
             name
-        };
-    });
+        }
+    })
 
     // draw each slice
     function draw() {
         let result = data.reduce((data, spec) => {
-            let result = slice(spec.boundingBox, video, spec);
-            data[spec.name] = result;
-            return data;
-        }, {});
+            let result = slice(spec.boundingBox, video, spec)
+            data[spec.name] = result
+            return data
+        }, {})
         /*
          {
          "time": "00:00:05",
@@ -146,150 +178,152 @@ function initCapture(video, slices) {
             /^\d{5}$/.test(result.speed) &&
             /^(?:\d{3}|\d{2}\.\d)$/.test(result.altitude)
         ) {
-          log(result);
-          socket.send('telemetry',result);
+            if (result.time === '00:00:00' && !startCollection) {
+                console.log('Started collection of data.')
+                startCollection = true
+            }
+            log(result)
+            if (params.useWebsocket) {
+                socket.send('telemetry', result)
+            }
         } else {
-          log('bad frame');
+            log('bad frame')
+            if (startCollection) {
+                console.log('bad frame', JSON.stringify(result))
+            }
         }
 
-        requestAnimationFrame(draw);
+        procFpsCnt++
+
+        requestAnimationFrame(draw)
     }
 
-    setTimeout(draw, 2000);
+    setTimeout(draw, 2000)
 }
-
-var classifierContext = document.querySelector('#classifier').getContext('2d');
-var debug = document.querySelector('#debug');
-var log = (data) => {
-    debug.innerHTML = JSON.stringify(data, null, 2);
-}
-
-var characters = [];
-var glyphs = '0123456789.:'.split('');
 
 function loadChars() {
     //copy image data to classifier canvas
-    classifierContext.drawImage(document.querySelector('#chars'),0,0);
+    classifierContext.drawImage(document.querySelector('#chars'), 0, 0)
     //load the pixel data in the characters array
-    var w = 192, h = 16;
-    var data = classifierContext.getImageData(0,0,w,h).data;
-    for (var i=0; i<data.length; i+=4) {
-        var col = (i / 4) % w;
-        var row = Math.floor((i / 4) / w);
-        var charIndex = Math.floor(col / 16);
-        var charCol = col % 16;
-        var pixelIndex = charCol + row * 16;
+    var w = 192, h = 16
+    var data = classifierContext.getImageData(0, 0, w, h).data
+    for (var i = 0; i < data.length; i += 4) {
+        var col = (i / 4) % w
+        var row = Math.floor((i / 4) / w)
+        var charIndex = Math.floor(col / 16)
+        var charCol = col % 16
+        var pixelIndex = charCol + row * 16
         if (!characters[charIndex]) {
-            characters[charIndex] = [];
+            characters[charIndex] = []
         }
-        characters[charIndex][pixelIndex] = data[i] / 255;
+        characters[charIndex][pixelIndex] = data[i] / 255
     }
 }
 
 //tries to classify 
 function classify({ctx, el}, slice) {
-    var imageData = ctx.getImageData(16 * slice.index, 23, 16, 16);
-    var data = imageData.data;
+    var imageData = ctx.getImageData(16 * slice.index, 23, 16, 16)
+    var data = imageData.data
     var overlaps = characters.map((characterData, index) => {
         let total = characterData.reduce((total, pixel, index) => {
-            let similarity = 1 - Math.abs(pixel - (data[index * 4] / 255));
+            let similarity = 1 - Math.abs(pixel - (data[index * 4] / 255))
             return total + similarity
-        }, 0);
+        }, 0)
         return {
             overlap: total / 256,
             index
         }
-    });
-    var sorted = overlaps.sort((a,b) => {
-        if (a.overlap == b.overlap) return 0;
-        return a.overlap > b.overlap? -1: 1;
-    });
-    var best = sorted[0];
-    var index = best.index;
+    })
+    var sorted = overlaps.sort((a, b) => {
+        if (a.overlap == b.overlap) return 0
+        return a.overlap > b.overlap ? -1 : 1
+    })
+    var best = sorted[0]
+    var index = best.index
 
     //draw the slice at the specified index to see how much they change
-    classifierContext.drawImage(el, 16*slice.index, 23, 16, 16, 16*index, 0, 16,16)
-    return glyphs[index];
+    classifierContext.drawImage(el, 16 * slice.index, 23, 16, 16, 16 * index, 0, 16, 16)
+    return glyphs[index]
 }
 
 // extracts frames and individual characters to the defined canvas
 // first step is to capture the area of interest to greyscale (upper half)
 // next step is to only crop out individual characters based on segmentation (lower half)
 function slice(boundingBox, video, spec) {
-    let baseX = 0;
-    let baseY = 23;
+    let baseX = 0
+    let baseY = 23
 
-    var {ctx, el} = spec;
-    var [x, y, w, h] = boundingBox;
+    var {ctx, el} = spec
+    var [x, y, w, h] = boundingBox
 
     // apply calibration
     x += baseX
     y += baseY
 
-    ctx.drawImage(video, x, y, w, h, 0, 0, w, h);
+    ctx.drawImage(video, x, y, w, h, 0, 0, w, h)
     //get it back as data, make bw and put it
-    var [imageData, slices] = toBlackAndWhite(ctx.getImageData(0, 0, w, h), boundingBox);
-    ctx.putImageData(imageData, 0, 0);
-    var result = '';
+    var [imageData, slices] = toBlackAndWhite(ctx.getImageData(0, 0, w, h), boundingBox)
+    ctx.putImageData(imageData, 0, 0)
+    var result = ''
     slices.forEach((slice, i) => {
         if (slice.start && slice.width) {
             //draw the slices as 16x16 images in the lower half of the canvas
-            ctx.drawImage(el, slice.start, 0, slice.width, h, i* 16, h, 16, 16);
+            ctx.drawImage(el, slice.start, 0, slice.width, h, i * 16, h, 16, 16)
             result += classify(spec, slice)
         }
     })
-    return result;
+    return result
 }
 
 //converts the umagedata to black and white pixels
 function toBlackAndWhite(imageData, boundingBox) {
-    var data = imageData.data;
-    var whitest = 0;
-    var blackest = 255;
+    var data = imageData.data
+    var whitest = 0
+    var blackest = 255
     //get a greyscale version of the data in the bounding box
     //storing the whitest and the blackest value in the process
     for (var i = 0; i < data.length; i += 4) {
-        var r = data[i];
-        var g = data[i + 1];
-        var b = data[i + 2];
-        var brightness = (3 * r + 4 * g + b) >>> 3;
-        whitest = Math.max(whitest, brightness);
-        blackest = Math.min(blackest, brightness);
-        var bw = brightness;
-        data[i] = bw;
-        data[i + 1] = bw;
-        data[i + 2] = bw;
+        var r = data[i]
+        var g = data[i + 1]
+        var b = data[i + 2]
+        var brightness = (3 * r + 4 * g + b) >>> 3
+        whitest = Math.max(whitest, brightness)
+        blackest = Math.min(blackest, brightness)
+        var bw = brightness
+        data[i] = bw
+        data[i + 1] = bw
+        data[i + 2] = bw
     }
-    var threshold = (whitest + blackest) * 0.4;
-    var segments = {cols: [], rows: []};
+    var threshold = (whitest + blackest) * 0.4
+    var segments = {cols: [], rows: []}
     //threshold the greyscale image in the middle of the range
     //storing segment boundaries in the meantime
     for (var i = 0; i < data.length; i += 4) {
-        var bw = br = data[i];
-        var range = 10;
+        var bw = br = data[i]
+        var range = 10
         if (br < (threshold - range)) {
-            bw = 0;
+            bw = 0
         }
         if (br > (threshold + range)) {
-            bw = 255;
+            bw = 255
         }
         //calculate segmentation
-        var col = (i / 4) % boundingBox[2];
+        var col = (i / 4) % boundingBox[2]
         if (segments.cols[col] === undefined) {
-            segments.cols[col] = 0;
+            segments.cols[col] = 0
         }
-        segments.cols[col] = segments.cols[col] + bw;
-        // segments.cols[col] = Math.max(segments.cols[col], bw);
-        data[i] = bw;
-        data[i + 1] = bw;
-        data[i + 2] = bw;
+        segments.cols[col] = segments.cols[col] + bw
+        // segments.cols[col] = Math.max(segments.cols[col], bw)
+        data[i] = bw
+        data[i + 1] = bw
+        data[i + 2] = bw
     }
     // visualize segments masks
-    // for (var i = 0; i < data.length; i += 4) {
-    //     var col = (i / 4) % boundingBox[2];
-    //     var isWhite = segments.cols[col] > 255;
+    // for (var i = 0 i < data.length i += 4) {
+    //     var col = (i / 4) % boundingBox[2]
+    //     var isWhite = segments.cols[col] > 255
     //     if (isWhite) {
-    //         data[i+1] = 255;
+    //         data[i+1] = 255
     //     }
     // }
 
@@ -302,21 +336,21 @@ function toBlackAndWhite(imageData, boundingBox) {
     // }
     // 
     var slices = segments.cols.reduce((slices, colValue, col) => {
-        var last = slices[slices.length-1];
-        var isWhite = colValue > 255; //1 bright white pixels minimum
+        var last = slices[slices.length - 1]
+        var isWhite = colValue > 255 //1 bright white pixels minimum
         if (isWhite && !last.start) {
-            last.start = col;
+            last.start = col
         }
         if (!isWhite && last.start) {
-            last.end = col;
-            last.width = last.end - last.start;
-            slices.push({index: slices.length});
+            last.end = col
+            last.width = last.end - last.start
+            slices.push({index: slices.length})
         }
-        return slices;
-    }, [{index: 0}]);
+        return slices
+    }, [{index: 0}])
 
-    imageData.data = data;
-    return [imageData, slices];
+    imageData.data = data
+    return [imageData, slices]
 }
 
 
@@ -330,20 +364,20 @@ function toBlackAndWhite(imageData, boundingBox) {
 // to see the data:
 // `mhub-client`
 function initWebSocket() {
-    ws = new WebSocket('ws://localhost:13900');
+    ws = new WebSocket('ws://localhost:13900')
 
     //subscribe to receive messages
-    ws.onopen = function() {
+    ws.onopen = function () {
         ws.send(JSON.stringify({
             type: 'subscribe',
             node: 'default'
-        }));
-    };
+        }))
+    }
 
     //handle messages received
-    ws.onmessage = function(msg) {
-        // console.log(JSON.parse(msg.data));
-    };
+    ws.onmessage = function (msg) {
+        // console.log(JSON.parse(msg.data))
+    }
 
     //send messages
     return {
@@ -353,7 +387,7 @@ function initWebSocket() {
                 node: 'default',
                 data: data,
                 topic: topic
-            }));
+            }))
         }
     }
 }
--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-const electron = require('electron');
-const ipc = electron.ipcMain;
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+"use strict"
 
-const path = require('path');
-const url = require('url');
-const fs = require('fs');
+const electron = require('electron')
+    , ipc = electron.ipcMain
+    // Module to control application life.
+    , app = electron.app
+    // Module to create native browser window.
+    , BrowserWindow = electron.BrowserWindow
+    , path = require('path')
+    , url = require('url')
+    , fs = require('fs')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 720, height: 350, resizable: false});
+console.log('PARAMS', process.argv)
 
-  mainWindow.setMenu(null);
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 720, height: 350, resizable: false})
 
-  // mainWindow.maximize();
-  // mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow.setMenu(null)
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    // pathname: path.join(__dirname, 'index.html'),
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools({
-    mode: 'detach'
-  })
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools({
+        mode: 'detach'
+    })
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+
+    ipc.on('started', function() {
+        console.log('Web View script started')
+        mainWindow.webContents.send('params', {
+            useWebsocket: true, // false
+        })
+    })
+
 }
 
 // This method will be called when Electron has finished
@@ -51,19 +58,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
